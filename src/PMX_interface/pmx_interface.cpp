46b79610e01361cef9b02066a10a4409da6f9cc6
Andrea Gotelli <63293648+aGotelli@users.noreply.github.com>
2023-09-27 09:04:20 +0200

    Update pmx_interface.cpp

--- src/PMX_interface/pmx_interface.cpp
+++ src/PMX_interface/pmx_interface.cpp
@@ -1,4 +1,4 @@
-#include "PMX_interface/pmx_interface.hpp"
+#include "pmx_interface.hpp"
 
 #include <iostream>
 #include <sys/socket.h>
@@ -19,11 +19,11 @@
 
 
 PMXInterface::PMXInterface(const unsigned int t_number_of_sensors,
-                        std::shared_ptr<bool> t_start_recording,
-                        std::shared_ptr<bool> t_StopDemos)
+                           std::shared_ptr<bool> t_start_recording,
+                           std::shared_ptr<bool> t_StopDemos)
     :   m_number_of_sensors(t_number_of_sensors),
-        m_start_recording(t_start_recording),
-        m_StopDemos(t_StopDemos)
+    m_start_recording(t_start_recording),
+    m_StopDemos(t_StopDemos)
 {
 
     m_samples_stack.clear();
@@ -36,8 +36,8 @@ PMXInterface::PMXInterface(const unsigned int t_number_of_sensors,
 
     // Set up the server address structure
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(m_tcp_ip_port);
-    inet_pton(AF_INET, m_tcp_ip_address.c_str(), &(serverAddress.sin_addr));
+    serverAddress.sin_port = htons(TCP_PORT);
+    inet_pton(AF_INET, TCP_IP.c_str(), &(serverAddress.sin_addr));
 
     // Bind socket to the specified IP and port
     if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
@@ -67,17 +67,14 @@ PMXInterface::~PMXInterface()
     // Close the server socket
     close(serverSocket);
 
-//    thread.join();
-
-    m_future.get();
+    thread.join();
 
 }
 
 
 void PMXInterface::startRecordingLoop()
 {
-//    thread = std::thread([this](){recordingLoop();});
-    m_future = std::future<void>(std::async(std::launch::async, [this](){recordingLoop();}));
+    thread = std::thread([this](){recordingLoop();});
 }
 
 
@@ -88,7 +85,7 @@ void PMXInterface::recordingLoop()
 
     std::cout << "Connection address: " << inet_ntoa(clientAddress.sin_addr) << std::endl;
 
-    char buffer[m_buffer_size];
+    char buffer[BUFFER_SIZE];
     unsigned int bytesReceived;
 
     Sample sample;
@@ -98,49 +95,31 @@ void PMXInterface::recordingLoop()
     std::vector<double> timestamps(2);
     std::string values;
 
-    size_t pos;
 
     const unsigned int size_double = sizeof(double);
-    unsigned int begin = 0;
-    unsigned int end = 0;
+
 
     m_start = std::chrono::high_resolution_clock::now();
     while (not *m_StopDemos) {
 
-        bytesReceived = recv(clientSocket, buffer, m_buffer_size, 0);
+        bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE, 0);
 
-        std::cout << buffer << std::endl;
-        std::cout << std::hex << buffer << std::endl;
+//        std::cout << buffer << std::endl;
+//        std::cout << std::hex << buffer << std::endl;
 
 
         if(bytesReceived > size_double*m_number_of_sensors){
-//            buffer[bytesReceived] = '\0';
+            //            buffer[bytesReceived] = '\0';
 
             if(*m_start_recording){
 
 
                 values = std::string(buffer);
 
-//                sample.data_buffer = values;
+                sample.data_buffer = values;
+                sample.time_stamp = std::chrono::high_resolution_clock::now();
 
-                std::cout << values << "\n";
-
-
-//                sample.time_stamp = std::chrono::high_resolution_clock::now();
-
-//                std::cout << "values : \n";
-//                for(unsigned int sensor=0; sensor<m_number_of_sensors; sensor++){
-
-//                    for(unsigned int index=0; index<m_sensor_message_size; index++){
-//                        begin = size_double*sensor;
-//                        end = begin + size_double;
-//                        std::cout << atof(values.substr(begin, end).c_str()) << ", ";
-//                    }
-
-//                }
-//                std::cout.flush();
-
-//                m_samples_stack.push_back( sample );
+                m_samples_stack.push_back( sample );
             }
 
 
@@ -158,49 +137,44 @@ void PMXInterface::getSamplesData(YAML::Node &t_PMX_node, Eigen::MatrixXd &t_PMX
 
     const unsigned int number_of_samples = m_samples_stack.size();
 
-    t_PMX_data = Eigen::MatrixXd(5, number_of_samples);
-    double F1;
-    double F2;
-    double time_1;
-    double time_2;
+    t_PMX_data = Eigen::MatrixXd::Zero(1 + 2*m_number_of_sensors, number_of_samples);
+
+
+    double F;
+    double time;
 
 
     for(unsigned int index = 0;const auto& sample : m_samples_stack){
 
+        const auto relative_time = std::chrono::high_resolution_clock::duration(sample.time_stamp - m_start).count()/1e9;
+
+        unsigned int row = 0;
+        t_PMX_data(row, index) = relative_time;
 
         std::istringstream iss(sample.data_buffer);
-
         std::string token;
 
+        for(unsigned int sensor=0; sensor<m_number_of_sensors; sensor++){
 
-        std::getline(iss, token, ':');
-        time_1 = std::stod(token);
-        std::getline(iss, token, '/');
-        F1 = std::stod(token);
+            std::getline(iss, token, ':');
+            F = std::stod(token);
+            std::getline(iss, token, '/');
+            time = std::stod(token);
 
-        std::getline(iss, token, ':');
-        time_2 = std::stod(token);
-        std::getline(iss, token);
-        F2 = std::stod(token);
+            t_PMX_data(++row, index) = time;
+            t_PMX_data(++row, index) = F;
+        }
 
+        index++;
 
-        const auto relative_time = std::chrono::duration<double, std::milli>(sample.time_stamp - m_start).count();
-
-
-
-        t_PMX_data.col(index++) << relative_time, time_1, F1, time_2, F2;
     }
 
     t_PMX_node["number_of_samples"] = number_of_samples;
+    t_PMX_node["number_of_sensors"] = m_number_of_sensors;
 
     t_PMX_node["data_order"].push_back("local_time");
-    t_PMX_node["data_order"].push_back("sensor_1_timestamp");
-    t_PMX_node["data_order"].push_back("sensor_1_data");
-    t_PMX_node["data_order"].push_back("sensor_2_timestamp");
-    t_PMX_node["data_order"].push_back("sensor_2_data");
+    t_PMX_node["data_order"].push_back("sensor_timestamp");
+    t_PMX_node["data_order"].push_back("sensor_data");
+
 
 }
-
-
-
-
